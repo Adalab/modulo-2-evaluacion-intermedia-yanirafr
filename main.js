76a758469b20ea7d890c4a2930240fd604030a8b
Yanira <yanirafr@gmail.com>
2020-10-13 09:48:01 +0200

    add innerhtml

--- main.js
+++ main.js
@@ -8,27 +8,40 @@ const count = document.querySelector(".js-count");
 console.log(count);
 const btn = document.querySelector(".js-btn");
 
+let trys = 0;
+
 
 const getRandom = function () {
     const random = parseInt(Math.random() * 100);
     return random;
 };
 
-function updateClick() {
+function guess() {
     const random = getRandom();
     console.log(random);
     const num = userNum.value;
     console.log(num);
+
+    trys += 1;
+    count.innerHTML = `Número de intentos = ${trys}`;
+
+    console.log(trys);
     
     if (random > num) {
         console.log("mayor");
+        clue.innerHTML = "Demasiado bajo."
     }
     if (random < num) {
         console.log("menor");
+        clue.innerHTML = "Demasiado alto."
     }
     if (random === num) {
         console.log("igual");
+        clue.innerHTML = "Has ganado campeona!!!"
     }
+
 }
 
-btn.addEventListener("click", updateClick);
+console.log(guess());
+
+btn.addEventListener("click", guess);
